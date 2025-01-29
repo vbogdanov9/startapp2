db24777e38618e41a04893073d43a273520d7749
seva <vbogdanov9@gmail.com>
2025-01-30 00:05:31 +0300

    adapt hero-iteration-4 section

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
 const emptyBlocks = document.querySelectorAll(".empty");
 console.log("emptyBlocks =", emptyBlocks);
+
+
+// burger
+const burger = document.querySelector('.burger');
+const lines = document.querySelectorAll('.burger__line');
+
+function toggleBurger() {
+  lines.forEach((line) => line.classList.toggle('active'));
+}
+
+burger.addEventListener('click', toggleBurger);
